db0ea2b5636224cec4dfed17afddee40649f773e
Syamjith Sasidharan <syamjithas@gmail.com>
2022-05-28 17:32:26 +0530

    generator

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
 import { MongoClient } from "mongodb";
-import customer from "./src/profile";
+import getNewCustomer from "./src/profile.js";
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 const client = new MongoClient(uri);
 
 async function retrive() {
@@ -12,7 +12,7 @@ async function retrive() {
     const movies = database.collection("movies");
     // Query for a movie that has the title 'The Room'
     const query = { title: "The Room" };
-    const movie = await movies.findOne(query, customer);
+    const movie = await movies.findOne(query, getNewCustomer);
     // since this method returns the matched document, not a cursor, print it directly
     console.log(movie);
   } finally {
@@ -21,23 +21,28 @@ async function retrive() {
 }
 // retrive().catch(console.dir);
 
-
-async function insert() {
+async function insert(record, starting) {
   try {
     await client.connect();
     const database = client.db("insertDB");
-    const haiku = database.collection("haiku");
+    const haiku = database.collection("cust1");
     // create a document to insert
-    const doc = {
-      title: "Record of a Shriveled Datum",
-      content: "No bytes, no problem. Just insert a document, in MongoDB",
+    let result, cust;
+    for (let index = 1; index <= record; index++) {
+      process.stdout.write(`${index}/${record}\r`);
+      result = await haiku.insertOne(getNewCustomer(index));
+      starting++;
     }
-    const result = await haiku.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
-insert().catch(console.dir);
+insert(50000, 1).catch(console.dir);
+insert(50000, 50001).catch(console.dir);
+insert(50000, 100001).catch(console.dir);
+insert(50000, 150001).catch(console.dir);
+insert(50000, 200001).catch(console.dir);
+insert(50000, 250001).catch(console.dir);
+// insert(50000, 300001).catch(console.dir);
+
 
--- src/profile.js
+++ src/profile.js
@@ -1,100 +1,153 @@
-var customer ={
-    "name": "SAM ALEX",
-    "whatsappNumber": "Not Specified",
-    "bodyType": "Not Specified",
-    "age": "28",
-    "complexion": "Fair",
-    "height": "Not Specified",
-    "physicalStatus": "Normal",
-    "weight": "Not Specified",
-    "hobbies": "Not Specified",
-    "interests": "Not Specified",
-    "bloodGroup": "Not Specified",
-    "maritalStatus": "Single",
-    "noOfChildren": "0",
-    "eatingHabits": "Not Specified",
-    "drinkingHabits": "Not Specified",
-    "aadharNumber": "Not Specified",
-    "describeInYourWords ": "",
-    "address": {
-        "presentAddress": "Not Specified",
-        "permanentAddress": "Not Specified",
-        "communicationAddress": "Not Specified",
-        "routeToResidence": "Not Specified"
-    },
-    "religionInformation": {
-        "religion": "Christian",
-        "caste": "Jacobite",
-        "casteNoBar": "No",
-        "star": "Not Specified",
-        "raasi": "Not Specified",
-        "typeOfJathakam": "Not Specified",
-        "religionUnionLocation": "Not Specified",
-        "religiousRequirements": "Not Specified"
-    },
-    "location": {
-        "country": "India",
-        "district": "Ernakulam",
-        "state": "Kerala",
-        "city": "Koothattukulam"
-    },
-    "professionalInformation": {
-        "education": "Not Specified",
-        "educationInDetail": "Not Specified",
-        "jobCategory": "Not Specified",
-        "jobCategoryInDetail": "Not Specified",
-        "jobLocation": "Not Specified",
-        "monthlyIncome": "Not Specified"
-    },
-    "familyDetails": {
-        "fathersName": "Not Specified",
-        "jobCategory": "Not Specified",
-        "jobInDetails": "Not Specified",
-        "jobDescription": "Not Specified",
-        "educationCategory": "Not Specified",
-        "educationDetails": "Not Specified",
-        "familyValue": "Not Specified",
-        "guardianIfAny": "Not Specified",
-        "brothers": "Married-0, Unmarried-0",
-        "jobProfileOfSibling": "Not Specified",
-        "motherName": "Not Specified",
-        "familyType": "Not Specified",
-        "motherTongue": "Malayalam",
-        "sisters": "Married-0, Unmarried-0",
-        "financialStatus": "Not Specified"
-    },
-    "horoscopeDetails": {
-        "birthHour": "Not Specified",
-        "birthMinute": "Not Specified",
-        "placeOfBirth": "Country Not Specified, Region Not Specified, Place Not Specified",
-        "dateOfBirth": "1 - Not Specified - 0",
-        "janmaSistaDasa": "Not Specified",
-        "janmaSistaDasaEnd": "0 (Day) - 0 (Month) - 0 (Year)",
-        "horoscopeFile": "Not Specified"
-    },
-    "otherInfo": {
-        "languagesKnown": "Not Specified",
-        "blog": "Not Specified",
-        "facebookLink": "Not Specified",
-        "otherSocialNetworkLinks": "Not Specified"
-    },
-    "Preferences": {
-        "age": "20 To 25",
-        "maritalStatus": "Single",
-        "educationCategory": "Any",
-        "educationDetail": "Any",
-        "jobCategory": "Any",
-        "jobDetail": "Any",
-        "religion": "Christian",
-        "casteNoBar": "No",
-        "caste": "Orthodox , Marthoma , Malankara , Jacobite , CSI , Caldiyan Syrian",
-        "matchingStar": "Any",
-        "typeOfJathakam": "Any",
-        "country": "India",
-        "state": "Kerala",
-        "district": "Ernakulam",
-        "whatYouAreLookingFor": ""
-    }
+import data from "../data/refdata.js";
+
+function getName(length) {
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
-export default customer;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randomItem(type) {
+  return data[type][getRandomInt(data[type].length)];
+}
+
+function randomCaste(type) {
+  return data["caste"][type][getRandomInt(data["caste"][type].length)];
+}
+function randomDistrict(type) {
+  let obj = data["district"][type]
+  return obj[getRandomInt(obj.length)];
+}
+
+function getSalary() {
+  return 15000 * getRandomIntInclusive(0, 25);
+}
+
+const getNewCustomer = (index) => {
+  let religion = randomItem("Religion"),
+    caste = randomCaste(religion),
+    education = randomItem("education"),
+    educationInDetail = randomItem("educationInDetail"),
+    jobCategory = randomItem("jobCategory"),
+    jobCategoryInDetail = randomItem("jobCategoryInDetail"),
+    state = randomItem("state");
+  return {
+    uniqId: index,
+    name: `${getName(10)} ${getName(10)}`,
+    whatsappNumber: "Not Specified",
+    bodyType: randomItem("BodyType"),
+    age: getRandomIntInclusive(18, 50),
+    complexion: randomItem("Complexion"),
+    height: randomItem("Height"),
+    physicalStatus: "Normal",
+    weight: randomItem("Weight"),
+    hobbies: "Not Specified",
+    interests: "Not Specified",
+    bloodGroup: randomItem("BloodGroup"),
+    maritalStatus: randomItem("MaritalStatus"),
+    noOfChildren: "0",
+    eatingHabits: randomItem("EatingHabits"),
+    drinkingHabits: randomItem("DrinkingHabits"),
+    aadharNumber: "Not Specified",
+    describeInYourWords: "",
+    address: {
+      presentAddress: "Not Specified",
+      permanentAddress: "Not Specified",
+      communicationAddress: "Not Specified",
+      routeToResidence: "Not Specified",
+    },
+    religionInformation: {
+      religion,
+      caste,
+      casteNoBar: "No",
+      star: randomItem("star"),
+      raasi: randomItem("raasi"),
+      typeOfJathakam: "Not Specified",
+      religionUnionLocation: "Not Specified",
+      religiousRequirements: "Not Specified",
+    },
+    location: {
+      country: randomItem("country"),
+      district: randomDistrict(state),
+      state,
+      city: "Koothattukulam",
+    },
+    professionalInformation: {
+      education,
+      educationInDetail,
+      jobCategory,
+      jobCategoryInDetail,
+      jobLocation: "Not Specified",
+      monthlyIncome: getSalary(),
+    },
+    familyDetails: {
+      fathersName: "Not Specified",
+      jobCategory: "Not Specified",
+      jobInDetails: "Not Specified",
+      jobDescription: "Not Specified",
+      educationCategory: "Not Specified",
+      educationDetails: "Not Specified",
+      familyValue: randomItem("familyvalues"),
+      guardianIfAny: "Not Specified",
+      brothers: "Married-0, Unmarried-0",
+      jobProfileOfSibling: "Not Specified",
+      motherName: "Not Specified",
+      familyType: "Not Specified",
+      sisters: "Married-0, Unmarried-0",
+      financialStatus: "Not Specified",
+    },
+    motherTongue: randomItem("languagesknown"),
+    horoscopeDetails: {
+      birthHour: "Not Specified",
+      birthMinute: "Not Specified",
+      placeOfBirth: "Country Not Specified",
+      dateOfBirth: "1 - Not Specified - 0",
+      janmaSistaDasa: "Not Specified",
+      janmaSistaDasaEnd: "",
+      horoscopeFile: "Not Specified",
+    },
+    Hobbies: ["Cycling", "Cricket", "Reading"],
+    interests: {
+      movie: ["horror"],
+      songs: ["classic"],
+    },
+    otherInfo: {
+      languagesKnown: "Not Specified",
+      blog: "Not Specified",
+      facebookLink: "Not Specified",
+      otherSocialNetworkLinks: "Not Specified",
+    },
+    Preferences: {
+      age: { min: 18, max: 28 },
+      maritalStatus: "Single",
+      educationCategory: "Any",
+      educationDetail: "Any",
+      jobCategory: "Any",
+      jobDetail: "Any",
+      religion: "Christian",
+      casteNoBar: "No",
+      caste: ["Orthodox", "Marthoma"],
+      matchingStar: "Any",
+      typeOfJathakam: "Any",
+      country: "India",
+      state: "Kerala",
+      district: "Ernakulam",
+      whatYouAreLookingFor: "I am looking for bla bla bla...",
+    },
+  };
+};
+
+export default getNewCustomer;
